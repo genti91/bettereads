5d10ded4de8fe713c12a6d874dd6403e778f27bf
amaruDuran <amaruduran@gmail.com>
2024-10-26 19:01:26 -0300

    add seed for user-test

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 import { books } from './books'
 
+
+const USER_ID_DEFAULT = 1;
 const prisma = new PrismaClient()
 
 async function main() {
+  const _user = await prisma.user.create({
+    data: {
+      username: "giovinco",
+      password: "asdf1234"
+    },
+  });
+
+
   for (let book of books) {
     await prisma.book.upsert({
         where: { id: book.id },
@@ -15,9 +25,10 @@ async function main() {
               where: { name: genreName },
               create: { name: genreName }
             }))
-          }
+          },
+          userId: USER_ID_DEFAULT,
         }
-    })
+    });
   }
 }
 
