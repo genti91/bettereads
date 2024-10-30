54dd33ad9ba7ef71170391fae2f904fbfbd7b131
Federico Genaro <fedegen14@gmail.com>
2024-10-30 12:45:05 -0300

    Log después de registro

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -77,6 +77,7 @@ export default function SignIn() {
                 description: "Usuario registrado"
             })
             form.reset();
+            await onSubmit(values);
             await revalidateAll();
         } else {
             toast({
