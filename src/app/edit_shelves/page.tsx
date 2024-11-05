167f162efc27bdfe4bd0c4b21c3f8f21904f205d
Franco <franco.g7@hotmail.com>
2024-11-05 14:50:09 -0300

    edit shelves

--- src/app/edit_shelves/page.tsx
+++ src/app/edit_shelves/page.tsx
@@ -2,23 +2,56 @@ import BookshelvesMenue from "@/components/BookshelvesMenue";
 import Books from "@/components/sections/Books";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { ShelfType, Shelve as PrismaShelve } from "@prisma/client";
 import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
+import { authOptions } from "../api/auth/[...nextauth]/authOptions";
+import EditShelf from "@/components/EditShelf";
 
-export default function EditShelves({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+interface Shelve extends PrismaShelve {
+    bookCount: number;
+}
+
+async function getShelves(userId: string) {
+    const response = await fetch(
+        `${process.env.APP_URL}/api/bookshelves?userId=${userId}`,
+        { cache: "no-store" }
+    );
+    if (!response.ok) {
+        throw new Error("Failed to fetch shelves");
+    }
+    return response.json();
+  }
+
+export default async function EditShelves({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return <div>Unauthorized</div>;
+    }
+    let defaultShelves: Shelve[];
+    let customShelves: Shelve[];
+    try {
+        let shelves = await getShelves(session.user.id);
+        defaultShelves = shelves.filter((shelve: Shelve) => shelve.type === ShelfType.DEFAULT);
+        customShelves = shelves.filter((shelve: Shelve) => shelve.type === ShelfType.CUSTOM);
+    } catch (error) {
+        console.error(error);
+        return <div>Failed to fetch shelves</div>;
+    }
   return (
     <div className="flex xl:px-80 px-10 py-10 justify-items-center sm:px-20 sm:py-10 font-[family-name:var(--font-geist-sans)]">
         <div className="flex flex-col gap-10 w-full">
             <h2 className="text-2xl font-bold">My Shelves</h2>
             <div className="flex flex-col w-2/3 self-center gap-2">
-                {Array.from({length: 5}, (_, i) => (
+                {customShelves.length === 0 && <div>No custom shelves</div>}
+                {[...customShelves].map( (shelve: Shelve) => (
                     <>
                         <Separator />
                         <div className="flex justify-between">
-                            <h3 className="text-xl font-bold">Custom 1</h3>
+                            <h3 className="text-xl font-bold">{shelve.name}</h3>
                             <div className="flex gap-2 mt-1">
-                                <TrashIcon className="h-5 w-5 cursor-pointer" />
-                                <Pencil1Icon className="h-5 w-5 cursor-pointer" />
+                                <EditShelf shelf={shelve}/>
                             </div>
                         </div>
                     </>
